813bc5cca7ab262f231edd8bd6884f6046f806fe
hazbon <50646522+ibumaxnet@users.noreply.github.com>
2021-04-09 12:57:15 +0900

    高度な型 intersection

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,9 @@
 "use strict";
+var e1 = {
+    name: "Alice",
+    priveleges: ['create-server'],
+    startDate: new Date(),
+    skill: ['jump'],
+};
+console.log(e1);
 //# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,55 @@
+// union型の場合共通の部分の型になる
+type Combinable = string | number | string;
+type Numeric = number | boolean | string;
+type ScriptString = string | number;
+
+type Universal = Combinable & Numeric & ScriptString ;
+// >> type Universal = string | number
+
+
+// オブジェクト型の場合複数の型を結合する
+type Admin = {
+	name: string;
+	priveleges: string[];
+}
+
+type Employee = {
+	name: string;
+	startDate: Date; //Date型 はtypescript に用意された日付型
+}
+
+type Skill = {
+	name: string;
+	skill: string[];
+}
+
+type ElevatedEmployee = Admin & Employee & Skill;
+
+
+const e1: ElevatedEmployee = {
+	name: "Alice",
+	priveleges: ['create-server'],
+	startDate: new Date(),
+	skill: ['jump'],
+}
+
+console.log(e1);
+// >>{"name": "Alice", "priveleges": [ "create-server" ], "startDate": "2021-04-09T03:48:29.064Z", "skill": [ "jump" ] }
+
+
+
+
+// interface で書くと...
+/*
+interface Admin = {
+	name: string;
+	priveleges: string[];
+}
+
+interface Employee = {
+	name: string;
+	startDate: Date;
+}
+
+interface ElevatedEmployee extends Admin, Employee {};
+*/

affbb996e4cab16f4d89cd31539c99048feb9e03
hazbon <50646522+ibumaxnet@users.noreply.github.com>
2021-04-15 10:25:08 +0900

    ジェネリックスについて

--- dist/app.js
+++ dist/app.js
@@ -1,96 +1,51 @@
 "use strict";
-var e1 = {
-    name: "Alice",
-    privileges: ['create-server'],
-    startDate: new Date(),
-    skill: ['jump'],
+var __spreadArray = (this && this.__spreadArray) || function (to, from) {
+    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
+        to[j] = from[i];
+    return to;
 };
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString;
-    }
-    return a + b;
+function merge(objA, objB) {
+    return Object.assign(objA, objB);
 }
-console.log(add(2, 5));
-function printEmployeeInformation(emp) {
-    console.log(emp.name);
-    if ('privileges' in emp) {
-        console.log("privileges: " + emp.privileges);
+var mergedObj = merge({ name: 'max' }, { age: 30, skills: ['director'] });
+console.log(mergedObj.skills);
+function countAndDescribe(element) {
+    var descriptionText = '値がありません';
+    if (element.length > 0) {
+        descriptionText = "\u5024\u306F " + element.length + " \u3067\u3059";
     }
-    if ('startDate' in emp) {
-        console.log("startDate: " + emp.startDate);
-    }
+    return [element, descriptionText];
 }
-printEmployeeInformation(e1);
-printEmployeeInformation({ name: 'usagi', startDate: new Date() });
-var Car = (function () {
-    function Car() {
+console.log(countAndDescribe('どうしてかした'));
+function extractAndConvert(obj, key) {
+    return 'value: ' + obj[key];
+}
+console.log(extractAndConvert({ "name": 'max' }, "name"));
+var DataStorage = (function () {
+    function DataStorage() {
+        this.data = [];
     }
-    Car.prototype.drive = function () {
-        console.log("運転中...");
+    DataStorage.prototype.addItem = function (item) {
+        this.data.push(item);
     };
-    return Car;
+    DataStorage.prototype.removeItem = function (item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    };
+    DataStorage.prototype.getItems = function () {
+        return __spreadArray([], this.data);
+    };
+    return DataStorage;
 }());
-var Track = (function () {
-    function Track() {
-    }
-    Track.prototype.drive = function () {
-        console.log("トラックスを運転中...");
-    };
-    Track.prototype.loadCargo = function (amount) {
-        console.log("\u8377\u7269\u3092 " + amount + " \u904B\u3093\u3067\u3044\u307E\u3059");
-    };
-    return Track;
-}());
-var v1 = new Car();
-var v2 = new Track();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Track) {
-        vehicle.loadCargo(1000);
-    }
+var textStorage = new DataStorage();
+textStorage.addItem('data1');
+textStorage.addItem(2);
+textStorage.removeItem('data1');
+console.log(textStorage.getItems());
+function createCouseGoal(title, description, date) {
+    var courseGoal = {};
+    courseGoal.title = title;
+    courseGoal.description = description;
+    courseGoal.completeUntil = date;
+    return courseGoal;
 }
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    var speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log(animal.type, speed);
-}
-moveAnimal({ type: 'horse', runningSpeed: 100 });
-var userInputElement = document.getElementById('user-input');
-if (userInputElement) {
-    userInputElement.value = 'こんちちは';
-}
-var errorBag = {
-    email: '正しいメールアドレスではありません',
-    username: 'ユーザー名に記号を含めることはできません',
-};
-function addOver(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString;
-    }
-    return a + b;
-}
-var result = addOver('hello', ' typescript');
-result.split(' ');
-var fechedUserData = {
-    id: 'u1',
-    name: 'user1',
-    job: {
-        title: 'developer',
-        description: 'TypeScript',
-    }
-};
-console.log(fechedUserData === null || fechedUserData === void 0 ? void 0 : fechedUserData.job.title);
-var userInput = null;
-var soterdData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
-console.log(soterdData);
 //# sourceMappingURL=app.js.map

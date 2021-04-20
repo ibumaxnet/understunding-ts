4b3b38f03a6146b84d1541121a7acc385cd8861d
hazbon <50646522+ibumaxnet@users.noreply.github.com>
2021-04-20 11:49:33 +0900

    デコレーターについて

--- src/app.ts
+++ src/app.ts
@@ -1,108 +1,223 @@
-// generics
-
-// extends で型の制約ができます
-// 型はオブジェクト型でなくても Union型や 配列型でもできます
-// T, U はオブジェクトでなくてはならない
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-	return Object.assign(objA, objB);
-}
-
-const mergedObj = merge<{name: string}, {age: number, skills: string[]}>({ name: 'max'}, { age: 30, skills: ['director']});
-
-console.log(mergedObj.skills);
-// >> ["director"]
-
-
-
-
-
-interface Lengthy {
-	length: number;
-}
-
-// length プロパティがあることを強制します
-function countAndDescribe<T extends Lengthy>(element: T) {
-	let descriptionText = '値がありません';
-	if(element.length > 0) {
-		descriptionText = `値は ${element.length} です`;
-	}
-	return [element, descriptionText];
-}
-
-console.log(countAndDescribe('どうしてかした'));
-// >> ["どうしてかした", "値は 7 です"]
-
-
-
-
-
-
-// keyof の制約
-// extends で <T>object に存在する key<U> であることを制限する
-function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
-	return 'value: ' + obj[key];
-}
-
-console.log(extractAndConvert({"name": 'max'}, "name"));
-
-
-
-
-
-
-
-
-// generic class
-class DataStorage<T extends string | number | boolean> {
-	private data: T[] = [];
-
-	addItem(item: T) {
-		this.data.push(item);
-	}
-
-	removeItem(item: T) {
-		this.data.splice(this.data.indexOf(item), 1);
-	}
-
-	getItems() {
-		return [...this.data];
+function Logger(logString: string) {
+		console.log('Loggerファクトリ');
+	return function(constructor: Function) {
+		console.log(logString);
+		console.log(constructor);
 	}
 }
 
-const textStorage = new DataStorage<string | number>(); // stringを格納できるインスタンス
- textStorage.addItem('data1');
- textStorage.addItem(2);
- textStorage.removeItem('data1');
-
- console.log(textStorage.getItems());
-
-
-
-
-//  Utility Types
-
- // pershal
- interface CourseGoal {
- 	title: string;
- 	description: string;
- 	completeUntil: Date;
- }
-
-function createCouseGoal(
-	title: string,
-	description: string,
-	date: Date,
-	): CourseGoal {
-	let courseGoal: Partial <CourseGoal> = {};
-	courseGoal.title = title;
-	courseGoal.description = description;
-	courseGoal.completeUntil = date;
-
-	return courseGoal as CourseGoal;
+function WithTemplate(template: string, hookId: string) {
+		console.log('templateファクトリ');
+	return function<T extends {new(...args: any[]): {name: string}}>(originalConstructor: T) {
+		return class extends originalConstructor {
+			constructor(..._: any[]) {
+				super();
+				console.log('テンプレート出力中');
+				const hookEl = document.getElementById(hookId);
+				if (hookEl) {
+					hookEl.innerHTML = template;
+					hookEl.querySelector('h1')!.textContent = this.name;
+				}
+			}
+		}
+	}
 }
 
-// Readonly
-const names: Readonly<string[]> = ['name', 'age'];
+@Logger('@ログ出力中')
+@WithTemplate('<h1>オブジェクト</h1>', 'app')
 
-names.push('height');// readonly なのでエラー
-// >> Property 'push' does not exist on type 'readonly string[]'.
+class Person {
+	name = 'max';
+	constructor() {
+		console.log('オブジェクトを作成中...');
+	}
+}
+
+const pers = new Person();
+
+console.log(pers);
+
+
+
+
+
+
+// ----------
+// デコレーターを追加できる位置
+
+function  Log(target: any, propertyName: string | symbol) {
+	console.log('Property デコレータ');
+	console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+	console.log('accessor デコレータ');
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+function Log3(target: any, name: string | symbol, descriptor: PropertyDescriptor) {
+	console.log('method デコレータ');
+	console.log(target);
+	console.log(name);
+	console.log(descriptor);
+}
+
+function Log4(target: any, name: string | symbol, position: number) {
+	console.log('parameter デコレータ');
+	console.log(target);
+	console.log(name);
+	console.log(position);
+}
+
+class Product {
+	@Log
+	title: string;
+	private _price: number;
+
+	@Log2
+
+	set price(val: number) {
+		if(val > 0) {
+			this._price = val;
+		} else {
+			throw new Error('不正な価格です 0以下は設定できません');
+		}
+	}
+
+	constructor(t: string, p: number) {
+		this.title = t;
+		this._price = p;
+	}
+
+	@Log3
+
+	getPriceWithTax(@Log4 tax: number) {
+		return this._price * (1 + tax);
+	}
+}
+
+
+
+
+
+
+
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+	const originalMethod = descriptor.value;
+	const adjDescriptor: PropertyDescriptor = {
+		configurable: true,
+		enumerable: false,
+		get() {
+			const boundFn = originalMethod;
+			return boundFn;
+		},
+	};
+	return adjDescriptor;
+}
+
+
+class Printer {
+	message = 'クリックしました';
+
+	@Autobind
+	showMessage() {
+		console.log(this.message);
+	}
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener('click', p.showMessage);
+
+
+
+
+
+
+
+
+// ----------
+
+interface ValidatorConfig {
+	[prop: string]: {
+		[validatableProp: string]: string[] // ['required', 'positive']
+	}
+}
+
+const registeredValidators: ValidatorConfig = {};
+
+function Required(target: any, propName: string) {
+registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "required",
+    ],
+  };}
+
+function PositiveNumber(target: any, propName: string) {
+  registeredValidators[target.constructor.name] = {
+    ...registeredValidators[target.constructor.name],
+    [propName]: [
+      ...(registeredValidators[target.constructor.name]?.[propName] ?? []),
+      "positive",
+    ],
+  };
+	}
+}
+function validate(obj: any) {
+	const objValidatorConfig = registeredValidators[obj.constructor.name];
+	if(!objValidatorConfig) {
+		return true;
+	}
+	let isValid = true;
+	for (const prop in objValidatorConfig) {
+		console.log(prop);
+		for (const validator of objValidatorConfig[prop] as string[]) {
+			switch (validator) {
+				case 'required':
+					isValid = isValid && !!obj[prop];
+					break;
+				case 'positive':
+					isValid = isValid && +obj[prop] > 0;
+					break;
+			}
+		}
+	}
+		return isValid;
+}
+
+
+class Course{
+	@Required
+	title: string;
+	@PositiveNumber
+	price: number;
+
+	constructor(t: string, p: number) {
+		this.title = t;
+		this.price = p;
+	}
+}
+
+const courseForm = document.querySelector('form')!;
+courseForm.addEventListener('submit', event => {
+	event.preventDefault();
+	const titleEl = document.getElementById('title') as HTMLInputElement;
+	const priceEl = document.getElementById('price') as HTMLInputElement;
+
+	const title = titleEl.value;
+	const price = +priceEl.value;
+
+	const createdCourse = new Course(title, price);
+
+	if (!validate(createdCourse)) {
+		alert('正しく入力してください');
+		return;
+	}
+	console.log(createdCourse);
+});
